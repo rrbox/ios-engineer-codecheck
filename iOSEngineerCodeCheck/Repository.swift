a7d91bd228daa79f01aaf33b347fdd2ccfdfc9df
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-22 00:26:27 +0900

    add `Repository` を `RepositoryView` に出力するための構造体を作成しました.

--- iOSEngineerCodeCheck/Repository.swift
+++ iOSEngineerCodeCheck/Repository.swift
@@ -36,3 +36,28 @@ extension Repositories {
         return result
     }
 }
+
+struct RepositoryDetailOutput {
+    var fullName: String
+    var language: String
+    var stargazersCount: String
+    var watchersCount: String
+    var forksCount: String
+    var openIssuesCount: String
+    var ownerImageURL: URL
+}
+
+extension RepositoryDetailOutput {
+    init(from repository: Repository) throws {
+        self.fullName = repository.fullName
+        self.language = "Written in \(repository.language ?? "other")"
+        self.stargazersCount = "\(repository.stargazersCount) stars"
+        self.watchersCount = "\(repository.watchersCount) watchers"
+        self.forksCount = "\(repository.forksCount) forks"
+        self.openIssuesCount = "\(repository.openIssuesCount) open issues"
+        guard let url = URL(string: repository.owner.avatarUrl) else {
+            throw RepositoryLoadError.imageURLLoadFailed
+        }
+        self.ownerImageURL = url
+    }
+}
