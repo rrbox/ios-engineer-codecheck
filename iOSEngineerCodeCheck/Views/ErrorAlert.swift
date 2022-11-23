964ece8453a42b330004af56ee9cff706ac59f95
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-24 01:45:26 +0900

    add エラー用アラートを作成する protocol を追加しました.
    `SingleButtonErrorAlert` と `ButtonLessErrorAlert` の二つの構造体を実装しました.

--- iOSEngineerCodeCheck/Views/ErrorAlert.swift
+++ iOSEngineerCodeCheck/Views/ErrorAlert.swift
@@ -5,4 +5,35 @@
 //  Created by rrbox on 2022/11/23.
 //
 
-import Foundation
+import UIKit
+
+protocol ErrorAlertCreate {
+    func create(title: String?,
+                message: String?) -> UIAlertController
+}
+
+struct SingleButtonErrorAlert: ErrorAlertCreate {
+    func create(title: String? = "Error", message: String? = "description") -> UIAlertController {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .default))
+        return alert
+    }
+}
+
+struct ButtonLessErrorAlert: ErrorAlertCreate {
+    func create(title: String? = "Error", message: String? = "description") -> UIAlertController {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            alert.dismiss(animated: true)
+        }
+        return alert
+    }
+}
