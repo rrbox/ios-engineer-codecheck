46168259a9b5895d73524114a66e6e78fc5c3bab
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-23 14:59:49 +0900

    add `ReposoitoryDetailOutput` の変換テストを追加しました.

--- iOSEngineerCodeCheckTests/ModelTests/RepositoryDetailTests.swift
+++ iOSEngineerCodeCheckTests/ModelTests/RepositoryDetailTests.swift
@@ -6,30 +6,23 @@
 //
 
 import XCTest
+@testable import iOSEngineerCodeCheck
 
 final class RepositoryDetailTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let repository = Repository(fullName: "test",
+                                stargazersCount: 0,
+                                watchersCount: 1,
+                                forksCount: 2,
+                                openIssuesCount: 3,
+                                owner: Owner(avatarUrl: "https://example"))
+    
+    func testOutputConvert() throws {
+        let output = try RepositoryDetailOutput(from: self.repository)
+        XCTAssertEqual(output.fullName, repository.fullName)
+        XCTAssertEqual(output.language, "Written in \(self.repository.language ?? "other")")
+        XCTAssertEqual(output.stargazersCount, "\(self.repository.stargazersCount) stars")
+        XCTAssertEqual(output.watchersCount, "\(self.repository.watchersCount) watchers")
+        XCTAssertEqual(output.forksCount, "\(self.repository.forksCount) forks")
+        XCTAssertEqual(output.openIssuesCount, "\(self.repository.openIssuesCount) open issues")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
