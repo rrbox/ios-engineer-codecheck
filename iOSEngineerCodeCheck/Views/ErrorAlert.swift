dd9d6b26712ee10e88b93c34a841d5735de1f499
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-24 01:50:31 +0900

    update `RepositorySearchError` に `PresentableError` を実装しました.

--- iOSEngineerCodeCheck/Views/ErrorAlert.swift
+++ iOSEngineerCodeCheck/Views/ErrorAlert.swift
@@ -52,3 +52,18 @@ struct ErrorAlert<E: PresentableError> {
         self.error = error
     }
 }
+
+extension RepositorySearchError: PresentableError {
+    func show(in parent: UIViewController) {
+        if self == .repositoriesArrayEmptyError {
+            parent.present(ButtonLessErrorAlert().create(
+                title: "検索結果",
+                message: "0件"), animated: true)
+        } else {
+            let alert = ButtonLessErrorAlert().create(message: self.localizedDescription)
+            parent.present(alert, animated: true)
+        }
+    }
+}
+
+
