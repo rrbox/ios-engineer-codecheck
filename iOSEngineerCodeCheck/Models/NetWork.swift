a8d0153ce91099e835826f0b11564b546125761e
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-24 23:31:10 +0900

    move `ObjectDownload` をどの層からも使用できる Commons に移動しました.

--- iOSEngineerCodeCheck/Models/NetWork.swift
+++ iOSEngineerCodeCheck/Models/NetWork.swift
@@ -13,36 +13,3 @@ enum GitHubAPI {
         URL(string: "https://api.github.com/search/repositories?q=\(query)")
     }
 }
-
-protocol Downloadable {}
-
-protocol DefaultDownloadable: Downloadable {
-    static func convert(from data: Data) throws -> Self
-}
-
-protocol OptionalDonwloadable: Downloadable {
-    static func convert(from data: Data) throws -> Self?
-}
-
-struct ObjectDownload<T: Downloadable> {
-    private let url: URL
-    
-    init(url: URL) {
-        self.url = url
-    }
-    
-}
-
-extension ObjectDownload where T: DefaultDownloadable {
-    func downloaded(_ session: URLSession = .shared) async throws -> T {
-        let (data, _) = try await session.data(for: URLRequest(url: url))
-        return try T.convert(from: data)
-    }
-}
-
-extension ObjectDownload where T: OptionalDonwloadable {
-    func downloaded(_ session: URLSession = .shared) async throws -> T? {
-        let (data, _) = try await session.data(for: URLRequest(url: url))
-        return try T.convert(from: data)
-    }
-}
