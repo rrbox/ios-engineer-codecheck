f1c63c7fff4354ebe20e0791a5ff776ce5513dbb
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-25 04:30:35 +0900

    add readme を取得する URL を生成する関数を追加しました.

--- iOSEngineerCodeCheck/Models/NetWork.swift
+++ iOSEngineerCodeCheck/Models/NetWork.swift
@@ -12,4 +12,9 @@ enum GitHubAPI {
     static func getSearchRepositoriesURL(query: String) -> URL? {
         URL(string: "https://api.github.com/search/repositories?q=\(query)")
     }
+    
+    /// README.md を取得する際に使用する関数です.
+    static func getReadmeURL(query: Repository) -> URL? {
+        URL(string: "https://api.github.com/repos/\(query.owner.login)/\(query.name)/readme")
+    }
 }
