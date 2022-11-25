636181a0e1e8133a1997acf07cfacc8adb25ef7c
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-25 11:02:19 +0900

    update UnitTest を更新しました.
    `RepositoryDetailOutput` の変更に伴い, テストの内容を変更しました.

--- iOSEngineerCodeCheckTests/ModelTests/RepositoryDetailTests.swift
+++ iOSEngineerCodeCheckTests/ModelTests/RepositoryDetailTests.swift
@@ -9,20 +9,23 @@ import XCTest
 @testable import iOSEngineerCodeCheck
 
 final class RepositoryDetailTests: XCTestCase {
-    let repository = Repository(fullName: "test",
+    let repository = Repository(name: "test_repository",
+                                fullName: "test",
                                 stargazersCount: 0,
                                 watchersCount: 1,
                                 forksCount: 2,
                                 openIssuesCount: 3,
-                                owner: Owner(avatarUrl: "https://example"))
+                                owner: Owner(login: "test_user",
+                                             avatarUrl: "https://example"))
     
     func testOutputConvert() throws {
         let output = try RepositoryDetailOutput(from: self.repository)
-        XCTAssertEqual(output.fullName, repository.fullName)
-        XCTAssertEqual(output.language, "Written in \(self.repository.language ?? "other")")
-        XCTAssertEqual(output.stargazersCount, "\(self.repository.stargazersCount) stars")
-        XCTAssertEqual(output.watchersCount, "\(self.repository.watchersCount) watchers")
-        XCTAssertEqual(output.forksCount, "\(self.repository.forksCount) forks")
-        XCTAssertEqual(output.openIssuesCount, "\(self.repository.openIssuesCount) open issues")
+        XCTAssertEqual(output.title, repository.name)
+        XCTAssertEqual(output.owner, repository.owner.login)
+        XCTAssertEqual(output.language, "\(self.repository.language ?? "other")")
+        XCTAssertEqual(output.stargazersCount, "\(self.repository.stargazersCount)")
+        XCTAssertEqual(output.watchersCount, "\(self.repository.watchersCount)")
+        XCTAssertEqual(output.forksCount, "\(self.repository.forksCount)")
+        XCTAssertEqual(output.openIssuesCount, "\(self.repository.openIssuesCount)")
     }
 }
